3398cb739bcb2485d7856699204ba6e92ce88437
Spyros Baltsas <sbaltsas@yahoo.gr>
2023-12-17 00:18:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Operating Systems - Git Hashing Exercise
+
+This repo contains my very simple bash script for obtaining the SHA-256 checksum of a 4 digit number. In order to test it, you must provide first with execution permission like so:
+
+``` sh
+$ chmod +x hash_script.sh
+```
+
+### Example usage
+
+``` sh
+$ ./hash_output.sh <your 4 digit number here>
+```
---
+++ hash_script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+argc=$#
+
+if [[ $argc -lt 1 ]]; then
+    echo "Error! At least one parameter must be provided!"
+    echo "[USAGE]: ./hash_script.sh <4-digit-number>"
+    exit 1
+fi
+
+if [[ ${#1} -lt 4 ]]; then
+    echo "Error! Your number must have at least 4 digits!"
+    exit 2
+fi
+
+output=$(echo -n "$1" | sha256sum)
+echo -n "$output" > hash_output.txt
